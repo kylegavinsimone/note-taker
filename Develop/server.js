6e1bcfaabe7998146cef9376af61667837679e71
kylegavinsimone <kylesimone@outlook.com>
2021-03-26 14:43:40 -0700

    testing if working now

--- Develop/server.js
+++ Develop/server.js
@@ -11,3 +11,4 @@ const PORT = process.env.PORT || 8888;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.listen(PORT, () => console.log("Listening on port: " + PORT));
